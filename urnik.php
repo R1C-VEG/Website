947ed9de03fb2b6c752ae925d7e3d8aefed29fe4
jujeKat <62479446+jujeKat@users.noreply.github.com>
2020-09-13 01:19:51 +0200

    Velik novih fancy ass stvari
    Vsi core fili so zdej PHP, da lahko zloudajo navbar is fila (navbar.html). glavn advantage tega je da nam ni treba za vsako spremembo v navbaru editat vsazga core fila posebi.
    
    Navbaru sm dodal mau stilističnih popravkov
         -barve ponekod
         -zradiru sm home button, in zdej vegova logo serva kot home button
         -razno
    
    Dodal sm nov tab "urnik":
        -basically samo iframe do easistenta ampak uporabla mal black magic javascripta k sm ga spizdu iz večinoma spizdu iz stackoverflowa da je responsive kinda in dela tut na telefonu (probably)

---
+++ urnik.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+  <head>
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title> R1C website </title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    
+    <style>
+
+/*Ta koda je za ozadje*/
+      body {
+        background-image: url("background.png");
+        background-repeat: no-repeat;
+        background-size: cover;
+      }
+
+/*Ta koda spreminja barvo bloka, ko greš čez njega*/
+      li a:hover {
+        background-color: #111;
+      }
+      
+/*ta gavno je za iframe za urnik*/
+      .skrij-se-prihaja-jujetova-koda{
+        margin:50px;
+        border-radius:15px;
+        border-style: solid;
+        border-color:#007BAC;
+        border-width: 5px;
+      }
+
+    </style>
+  </head>
+  <body>
+
+
+<!--Ta koda zloada navbar.html file da nam ni treba za vsako spremembo pr navbaru editat vseh html filou-->
+  <?php
+    require 'navbar.html';
+  ?>
+
+
+
+
+
+
+<!--tale neurejena, complicated, napol ukradena koda zloada urink iz urnik.vegova.si in ga automatsko adjusta na screen size. probably. -->
+
+<div class="skrij-se-prihaja-jujetova-koda">
+  <iframe src="https://www.easistent.com/urniki/30a1b45414856e5598f2d137a5965d5a4ad36826/razredi/424558" onload='javascript:(function(o){o.style.height=o.contentWindow.document.body.scrollHeight+"px";}(this));' style="height:600px;width:100%;border:none;overflow:hidden;"></iframe>
+</div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+  </body>
+</html>
