13e4f4436c16496fc75b2ef234bad48e015d975c
jujeKat <62479446+jujeKat@users.noreply.github.com>
2020-09-17 01:19:01 +0200

    Dodan Google Analytics

--- urnik.php
+++ urnik.php
@@ -2,6 +2,20 @@
 <html>
   <head>
 
+<!-- Goooooooogle špijoniraaa iz temmmm, ne tega dat iz heada - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-173502320-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-173502320-2');
+</script>
+
+
+
+
+
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
