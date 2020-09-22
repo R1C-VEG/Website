df9f725fb787bd32bf54191a9946623c2951d805
jujeKat <62479446+jujeKat@users.noreply.github.com>
2020-09-22 18:17:03 +0200

    Spremenu token acquiring script
    Če je tko narjen lahko js v linuxu naštimam crontab (service) ki avtomatsko to nardi, in manj cpuja požre pa še avtomatsko se recovera če se nekak crasha

--- Python-Scripts/eAuth.py
+++ Python-Scripts/eAuth.py
@@ -2,17 +2,8 @@
 
 from eAsistent import getNewToken
 from os import environ
-from time import sleep
 
-
-def getToken():
-    while True:
-        auth = getNewToken(environ.get("EA_Uporabnik"), environ.get("EA_Geslo"))
-            
-        with open("auth.txt", "w") as f:
-            f.write(f"{auth[0]}\n")
-            f.write(auth[1])
-
-        sleep(43200)
-
-getToken()
+auth = getNewToken(environ.get("EA_Uporabnik"), environ.get("EA_Geslo"))         
+with open("auth.txt", "w") as f:
+      f.write(f"{auth[0]}\n")
+      f.write(auth[1])
