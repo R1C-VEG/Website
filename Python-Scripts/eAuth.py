87df6108871aaa2e2290a465494ce8befe187d58
jujeKat <62479446+jujeKat@users.noreply.github.com>
2020-09-22 19:44:37 +0200

    oops
    eh k da kdo to bere

--- Python-Scripts/eAuth.py
+++ Python-Scripts/eAuth.py
@@ -3,11 +3,11 @@
 from eAsistent import getNewToken
 from os import environ
 
-with open("/home/r1c/stvari/koda.txt", "r") as koda:
+with open("koda.txt", "r") as koda:
       koda = koda.read()
 
 
 auth = getNewToken(environ.get("EA_Uporabnik"), koda)         
-with open("/home/r1c/stvari/auth.txt", "wb") as f:
+with open("auth.txt", "wb") as f:
       f.write(bytes("{}{}".format(auth[0], "\n"), "UTF-8"))
       f.write(auth[1])
