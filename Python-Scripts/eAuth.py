80acb3b44e0c130f9d7321a51add919388ce1cd2
jujeKat <62479446+jujeKat@users.noreply.github.com>
2020-09-24 09:58:10 +0200

    Update eAuth.py

--- Python-Scripts/eAuth.py
+++ Python-Scripts/eAuth.py
@@ -3,11 +3,14 @@
 from eAsistent import getNewToken
 from os import environ
 
+with open("mail.txt", "r") as mail:
+      mail = str(mail.read()).replace("\n", "")
+      
 with open("koda.txt", "r") as koda:
       koda = str(koda.read()).replace("\n", "")
 
 
-auth = getNewToken(str(environ["EA_Uporabnik"]), koda)         
+auth = getNewToken(mail, koda)         
 with open("auth.txt", "wb") as f:
       f.write(bytes("{}{}".format(auth[0], "\n"), "UTF-8"))
       f.write(bytes(auth[1], "UTF-8"))
